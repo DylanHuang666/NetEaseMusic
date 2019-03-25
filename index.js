eab4e5a13cade77cb0f9f43fdf5c84e12504fe18
Dylan <huanglinjay123@outlook.com>
2019-03-25 16:39:05 +0800

    加载动画，动态创建li，点击不同歌曲播放不同歌曲

---
+++ index.js
@@ -0,0 +1,30 @@
+$(function(){
+    $.get('./songs.json').then(function(response){
+        let items=response
+        items.forEach((i)=>{
+            let $li=$(`
+            <li>
+                <a href="./song.html?id=${i.id}">
+                   <h3>${i.name}</h3>
+                    <p>
+                        <svg class="icon-sq" aria-hidden="true">
+                            <use xlink:href="#icon-sq"></use>
+                        </svg>
+                        歌手-专辑
+                    </p>              
+                    <svg class="icon-play-circled" aria-hidden="true">
+                        <use xlink:href="#icon-play-circled"></use>
+                    </svg>              
+                </a>               
+            </li>
+            `)
+            $('#lastestMusic').append($li)
+        })
+        $('#lastestMusicLoading').remove()
+    })
+})
+
+
+
+
+
--- song.js
+++ song.js
@@ -1,4 +1,26 @@
 $(function(){
+
+    let id=parseInt(location.search.match(/\bid=([^&]*)/)[1],10)
+    $.get('./songs.json').then(function(response){
+        let songs=response
+        let song = songs.filter(s=>s.id === id)[0]
+        let {url}=song
+        let audio=document.createElement('audio') 
+        audio.src=url
+        audio.oncanplay=function(){
+            audio.play()
+            $('.disc-container').addClass('playing')
+        }
+        $('.icon-pause').on('click',function(){
+            audio.pause()
+            $('.disc-container').removeClass('playing')
+        })
+        $('.icon-play').on('click',function(){
+            audio.play()
+            $('.disc-container').addClass('playing')
+        })
+    })
+
     $.get('/lyric.json').then(function(object){
         let {lyric}=object
         let array=lyric.split('\n')
@@ -17,18 +39,5 @@ $(function(){
             $p.appendTo($lyric.children('.lines'))
         })
     })
-    let audio=document.createElement('audio') 
-    audio.src='http://pnksng3qv.bkt.clouddn.com/%E9%99%88%E5%A5%95%E8%BF%85%20-%20%E5%8D%81%E5%B9%B4.mp3'
-    audio.oncanplay=function(){
-        audio.play()
-        $('.disc-container').addClass('playing')
-    }
-    $('.icon-pause').on('click',function(){
-        audio.pause()
-        $('.disc-container').removeClass('playing')
-    })
-    $('.icon-play').on('click',function(){
-        audio.play()
-        $('.disc-container').addClass('playing')
-    })    
+        
 })
