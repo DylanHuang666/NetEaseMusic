123281aa6b60ecc05b5a5017e2f04bb43f818600
Dylan <huanglinjay123@outlook.com>
2019-03-26 02:07:50 +0800

    显示歌词到页面

--- song.js
+++ song.js
@@ -4,7 +4,17 @@ $(function(){
     $.get('./songs.json').then(function(response){
         let songs=response
         let song = songs.filter(s=>s.id === id)[0]
-        let {url}=song
+        let {url,name,lyric}=song
+        initPlayer.call(undefined,url)
+        initText(name,lyric)
+    })
+
+    function initText(name,lyric){//显示歌名和歌词
+        $('.song-description > h1').text(name)//显示歌名
+        parseLyric.call(undefined,lyric)//显示歌词
+    }
+
+    function initPlayer(url){ //歌曲初始播放
         let audio=document.createElement('audio') 
         audio.src=url
         audio.oncanplay=function(){
@@ -19,10 +29,9 @@ $(function(){
             audio.play()
             $('.disc-container').addClass('playing')
         })
-    })
+    }
 
-    $.get('/lyric.json').then(function(object){
-        let {lyric}=object
+    function parseLyric(lyric){//获取歌词
         let array=lyric.split('\n')
         let regex=/^\[(.+)\](.*)/
         array=array.map(function(string,index){
@@ -38,6 +47,7 @@ $(function(){
             $p.attr('data-time',object.time).text(object.words)
             $p.appendTo($lyric.children('.lines'))
         })
-    })
-        
+    }   
+
 })
+
